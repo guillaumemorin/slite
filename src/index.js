42e4e15b0903a89a1502451c350b3c570b80dcea
Guillaume Morin <gmorin.pro@gmail.com>
2018-06-07 14:52:02 +0200

    Add data input read function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // Your code goes here
 import net from 'net';
 
-const server = net.createServer((socket) => {
-	socket.write('TCP server running ... \r\n');
-	socket.pipe(socket);
+const server = net.createServer(socket => {
+    socket.write('TCP server running ... \r\n');
+    socket.pipe(socket);
+    socket.on('data', data => {
+        var read = data.toString();
+        console.log('data', read);
+    });
+});
+
+server.on('error', err => {
+    throw err;
 });
 
 server.listen(1337, '127.0.0.1');
