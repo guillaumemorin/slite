8bd83570e770f0206e40374250c75db4c38be8fd
Guillaume Morin <gmorin.pro@gmail.com>
2018-06-07 14:38:45 +0200

    Set tcp server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+import net from 'net';
+
+const server = net.createServer((socket) => {
+	socket.write('TCP server running ... \r\n');
+	socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
